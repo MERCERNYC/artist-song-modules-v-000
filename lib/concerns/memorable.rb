072bb6f167e07b858d975762c6e4f85f4e900829
Juliana Fogg <julianafogg@gmail.com>
2017-11-13 16:37:39 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,10 +1,10 @@
 module Memorable
 
-  def reset_all
+  def reset_all #can reset the artists that have been created
       self.all.clear
     end
 
-    def count
+    def count #can count how many artists have been created
       self.all.count
     end
 end
